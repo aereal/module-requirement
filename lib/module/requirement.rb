76f513efcbdea863fff56e4c530ca17e5208830e
aereal <aereal@aereal.org>
2014-12-14 01:51:49 +0900

    Friendly error messages

--- lib/module/requirement.rb
+++ lib/module/requirement.rb
@@ -2,13 +2,22 @@ require 'set'
 
 module Module::Requirement
   class Error < ::StandardError; end
-  class Error::NotMeetRequirements < Error; end
+  class Error::NotMeetRequirements < Error
+    def initialize(checked_class, unimplemented_methods)
+      @checked_class         = checked_class
+      @unimplemented_methods = unimplemented_methods
+    end
+
+    def message
+      "#{@checked_class} must implement required methods: #{@unimplemented_methods.join(', ')}"
+    end
+  end
 
   def self.included(included)
     included.extend(Declaration)
     included.singleton_class.send(:define_method, :included) do |inc|
       unimplemented = requirements.reject {|req| inc.method_defined?(req) }
-      raise Error::NotMeetRequirements, unimplemented unless unimplemented.empty?
+      raise Error::NotMeetRequirements.new(inc, unimplemented) unless unimplemented.empty?
     end
   end
 
